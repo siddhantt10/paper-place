bef6fc770f88bd039ecd332c797837bbbe9a01c4
Siddhant Patel <96189956+siddhantt10@users.noreply.github.com>
2023-01-17 21:09:20 +0530

    React-app setup with home route and about route

---
+++ src/About.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./About.css";
+
+function About() {
+  return (
+    <div className="app">
+      <h1>About</h1>
+      <h3>Under development :)</h3>
+    </div>
+  );
+}
+
+export default About;
---
+++ src/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+
+function Home() {
+  return (
+    <div className="home">
+      <h1>paperplace</h1>
+      <h3>Under development :)</h3>
+    </div>
+  );
+}
+
+export default Home;
